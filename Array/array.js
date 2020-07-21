6eb3c34e84f73b57dcea426cdef10aa14f9a5dcc
Kavisha Modi <38474385+kavishamodi97@users.noreply.github.com>
2020-07-21 18:27:18 +0530

    arrays and functions examples

---
+++ Array/array.js
@@ -0,0 +1,12 @@
+// use of Array
+var guestList=["Kavisha","Tanay","Mayank","Sumitra","Hasmukh","Jigna"];
+console.log(guestList);
+console.log(guestList.length);
+console.log(guestList[1]);
+
+if(guestList.includes("r")){
+ console.log("Welcome for party");
+}
+else{
+   console.log("Sorry you are not invited for party");
+}
